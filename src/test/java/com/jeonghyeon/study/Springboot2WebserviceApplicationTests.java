a2b05e19f3fa3479804b2e1cf4c8b550344e65c5
jeonghyeonkwon <givejeong2468@gmail.com>
2022-07-30 16:43:39 +0900

    배포 위해 일단 테스트 주석처리

--- src/test/java/com/jeonghyeon/study/Springboot2WebserviceApplicationTests.java
+++ src/test/java/com/jeonghyeon/study/Springboot2WebserviceApplicationTests.java
@@ -3,11 +3,11 @@ package com.jeonghyeon.study;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Springboot2WebserviceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
